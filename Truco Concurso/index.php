a840636c126253de97c17dcec9b0af11762680ef
Fryntiz <fryntiz@gmail.com>
2016-12-18 19:11:26 +0100

    agregada descripción y botón para comenzar evento

--- Truco Concurso/index.php
+++ Truco Concurso/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <HTML lang="es">
 <HEAD>
-	<TITLE>Nombre</TITLE>
+	<TITLE>Concurso para Ganar un Coche</TITLE>
 	<META charset="utf-8"/>
 	<META name="description" content="Plantilla de Ejemplo"/>
-	<META name="keywords" content="ejemplo, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
+	<META name="keywords" content="concurso, coche, ganar, plantilla, programación, Raúl Caro Pastorino, Fryntiz"/>
 	<META name="author" content="Raúl Caro Pastorino"/>
 	<LINK rel="shortcut icon" href="./images/favicon.png"/>
 	<LINK rel="stylesheet" href="./CSS/styles.css"/>
@@ -12,11 +12,15 @@
 </HEAD>
 
 <BODY>
-	<H1 id="titulo1">Título de la página</H1>
-	<H2 id="titulo2">Sección de que se trata</H2>
-	<P>Descripción</P>
+	<H1 id="titulo1">Concurso para Ganar un Coche</H1>
+	<H2 id="titulo2">Participa en el concurso para ganar un coche</H2>
+	<P>Pulsa sobre concursar para participar en el concurso</P>
 	<DIV class="ejemplo">
-		Plantilla de ejemplo
+		Solo tienes que participar y el coche puede ser tuyo:
+		<BR/>
+		<FORM name="concurso">
+			<INPUT type="button" name="concursar" value="Concursar"/>
+		</FORM>
 	</DIV>
 <?php
 
