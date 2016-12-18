69412070c70f37745e1e0c897e0a5adb85931832
Fryntiz <fryntiz@gmail.com>
2016-12-18 19:25:19 +0100

    Añadido script para esquivar ventana al hacer hover sobre "si" y mostrar mensaje al pulsar "no"

--- Truco Concurso/JS/scripts.js
+++ Truco Concurso/JS/scripts.js
@@ -1,4 +1,21 @@
 function concursando() {
-		// Abrimos una ventana con la página del premio
-		open("premio.html", "concurso", "width=250,height=80,status=0");
-	}
+	// Abrimos una ventana con la página del premio
+	open("concurso.html", "concurso", "width=350,height=200,status=0");
+}
+
+function esquivarUsuario() {
+	// Obtenemos la hora actual
+	var ahora = new Date();
+	// Calculamos las nuevas coordenadas
+	var x = ahora.getMilliseconds();
+	var y = x / 2;
+	// Desplazamos la ventana
+	window.moveTo(x, y);
+}
+
+function mostrarMensaje() {
+	// Mostra un mensaje al pulsar sobre no
+	alert("¡Nos sorprende su respuesta!");
+	// Cerrar la ventana actual
+	window.close();
+}
