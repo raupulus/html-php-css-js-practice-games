1cf08c05b7bd985537cfe719a0018bdac1d0b219
Fryntiz <tecnico@fryntiz.es>
2017-06-04 11:19:10 +0200

    Estructura básica

---
+++ Serpiente/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<HTML lang="es">
+<HEAD>
+	<TITLE>Serpiente</TITLE>
+	<META charset="utf-8" />
+
+	<META name="author" lang="es" content="Raúl Caro Pastorino, tecnico@fryntiz.es" />
+	<META name="organization" content="fryntiz" />
+	<META name='copyright' content='Serpiente' />
+	<META name='language' content='ES' />
+	<META name="robots" content="index, follow" />
+	<META name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+	<META property='fb:admins' content='fryntiz' />
+
+	<META property="og:locale" content="es_ES" />
+	<META property="og:locale:alternate" content="es_LA" />
+	<META property='og:url' content='http://www.fryntiz.es/' />
+	<META property='og:site_name' content='Serpiente' />
+	<META property='og:type' content='website' />
+
+	<META name="twitter:card" content="summary" />
+	<META name="twitter:site" content="@fryntiz" />
+	<META name="twitter:creator" content="@fryntiz" />
+	<META name='twitter:image' content='http://www.fryntiz.es/images/logo.png' />
+
+	<LINK rel="shortcut icon" href="./images/favicon.png"/>
+	<LINK href="CSS/styles.css" rel="stylesheet" type="text/css" media="screen" />
+	<LINK href="CSS/high.css" rel="stylesheet" type="text/css" media="screen and (min-width:981px)" />
+	<LINK href="CSS/medium.css" rel="stylesheet" type="text/css" media="screen and (min-width: 481px) and (max-width: 980px)" />
+	<LINK href="CSS/mini.css" rel="stylesheet" type="text/css" media="screen and (max-width: 480px)" />
+	<SCRIPT src="JS/scripts.js"></SCRIPT>
+	<SCRIPT src="JS/jquery.js"></SCRIPT>
+</HEAD>
+
+<BODY>
+
+</BODY>
+</HTML>
