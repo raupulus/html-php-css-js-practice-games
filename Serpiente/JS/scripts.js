221383e7206457552354c70c2f27bde541c452d5
Fryntiz <tecnico@fryntiz.es>
2017-06-04 11:25:35 +0200

    Creado archivo de scripts

---
+++ Serpiente/JS/scripts.js
@@ -0,0 +1,3 @@
+function versionActual() {
+	alert("La versión actual de este juego es la v.01 y está en pruebas\n Puedes disfrutar del juego pero puede contener errores que corregiré si me los reportas");
+}
