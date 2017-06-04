df28eade2f1a253fc859cd6fd931012d092b2b78
Fryntiz <tecnico@fryntiz.es>
2017-06-04 12:04:42 +0200

    Primeras variables

--- Serpiente/JS/scripts.js
+++ Serpiente/JS/scripts.js
@@ -1,3 +1,15 @@
+/*****************************************************
+  Creado por: Raúl Caro Pastorino --> www.fryntiz.es
+*****************************************************/
+
 function versionActual() {
 	alert("La versión actual de este juego es la v.01 y está en pruebas\n Puedes disfrutar del juego pero puede contener errores que corregiré si me los reportas");
 }
+
+/*************************
+				 VARIABLES
+**************************/
+var container = document.getElementById("cajasSerpiente"); //Caja del juego
+var points = document.getElementById("cajaPuntuacion"); //Caja Contador de puntos
+var Distance = document.getElementById("cajaTiempo"); //Caja Contador de tiempo
+
