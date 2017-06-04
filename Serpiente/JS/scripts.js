b0ade77b3a24961bde287a51443f01db29db9dd6
Fryntiz <tecnico@fryntiz.es>
2017-06-04 13:11:00 +0200

    Añadida función para generar cuadrículas

--- Serpiente/JS/scripts.js
+++ Serpiente/JS/scripts.js
@@ -9,7 +9,21 @@ function versionActual() {
 /*************************
 				 VARIABLES
 **************************/
-var container = document.getElementById("cajasSerpiente"); //Caja del juego
+var container = document.getElementById("cajaSerpiente"); //Caja del juego
 var points = document.getElementById("cajaPuntuacion"); //Caja Contador de puntos
 var Distance = document.getElementById("cajaTiempo"); //Caja Contador de tiempo
+var sheriff = document.getElementsByClassName("cuadricula"); //Array con cada cuadro
 
+// Variable que crea un array de cuadros con los que controlar movimientos
+var cuadriculas = function() {
+	var x = " ";
+
+	for (var i=1; i<=10000; ++i) {
+		x = x + "<DIV class=\"cuadricula\" id=\"b" + i + " \" ></DIV>"
+	}
+
+	container.innerHTML = x;
+	oggy = sheriff[1];
+};
+
+cuadriculas();
